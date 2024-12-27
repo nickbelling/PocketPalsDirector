be6326981f7e5d26e753470a8b568c1562271c0c
nickbelling <6581760+nickbelling@users.noreply.github.com>
2024-12-27 13:41:16 +1100

    Cleanup auth guard and redirection

--- src/app/common/auth/admin-auth-guard.ts
+++ src/app/common/auth/admin-auth-guard.ts
@@ -1,5 +1,10 @@
 import { inject, Injectable } from '@angular/core';
-import { CanActivate, Router } from '@angular/router';
+import {
+    ActivatedRouteSnapshot,
+    CanActivate,
+    Router,
+    RouterStateSnapshot,
+} from '@angular/router';
 import { AuthService } from './auth';
 
 @Injectable({
@@ -9,11 +14,25 @@ export class AdminAuthGuard implements CanActivate {
     private _auth = inject(AuthService);
     private _router = inject(Router);
 
-    public canActivate(): boolean {
+    public async canActivate(
+        _: ActivatedRouteSnapshot,
+        state: RouterStateSnapshot,
+    ): Promise<boolean> {
+        // Ensure the first check of the sign-in state has occurred.
+        await this._auth.waitForAuthInit();
+
         if (this._auth.isAdmin()) {
+            // Can access this route.
             return true;
         } else {
+            // Set the redirect URL to come back to once they successfully
+            // sign-in as an admin
+            this._auth.setRedirectUrl(state.url);
+
+            // Boot them to the 403 page
             this._router.navigate(['/', 'admin', '403']);
+
+            // Prevent visiting this guarded route
             return false;
         }
     }
--- src/app/common/auth/auth.ts
+++ src/app/common/auth/auth.ts
@@ -1,4 +1,11 @@
-import { DestroyRef, inject, Injectable, signal } from '@angular/core';
+import {
+    computed,
+    DestroyRef,
+    effect,
+    inject,
+    Injectable,
+    signal,
+} from '@angular/core';
 import { Router } from '@angular/router';
 import {
     GoogleAuthProvider,
@@ -7,6 +14,7 @@ import {
     signOut,
     User,
 } from 'firebase/auth';
+import { firstValueFrom, ReplaySubject } from 'rxjs';
 import { AUTH } from '../firestore/tokens';
 
 @Injectable({
@@ -16,40 +24,105 @@ export class AuthService {
     private _auth = inject(AUTH);
     private _router = inject(Router);
     private _destroyRef = inject(DestroyRef);
+    private _loadedWaiter = new ReplaySubject<void>(1);
+    private _redirectUrl?: string;
 
     public user = signal<User | null>(null);
+    public isSignedIn = computed(() => this.user() !== null);
     public isAdmin = signal<boolean>(false);
+    public isLoaded = signal<boolean>(false);
 
     constructor() {
+        // Monitor Firebase's auth change event
         const unsubscribeAuth = onAuthStateChanged(
             this._auth,
             async (user: User | null) => {
+                // Set the user (even if it's null, i.e. they have signed out)
                 this.user.set(user);
 
                 if (user) {
+                    // User has just signed in.
                     const token = await user.getIdTokenResult();
                     if (token.claims['admin']) {
+                        // User is an admin
                         this.isAdmin.set(true);
                     }
-                    this._router.navigate(['/', 'admin']);
                 } else {
+                    // User is signed-out, definitely not an admin
                     this.isAdmin.set(false);
                 }
+
+                this.isLoaded.set(true);
             },
         );
+
+        // Monitor the isLoaded signal for changes, and complete the
+        // _loadedWaiter subject. This will only ever happen once, as isLoaded
+        // is only ever set to true.
+        effect(() => {
+            const isLoaded = this.isLoaded();
+
+            if (isLoaded) {
+                this._loadedWaiter.next();
+                this._loadedWaiter.complete();
+            }
+        });
+
+        // When the user is successfully set, if a previous redirect URL was
+        // stored, navigate to it.
+        effect(() => {
+            const user = this.user();
+
+            if (user) {
+                if (this._redirectUrl) {
+                    this._router.navigateByUrl(this._redirectUrl);
+                    this._redirectUrl = undefined;
+                }
+            }
+        });
 
         this._destroyRef.onDestroy(() => {
             unsubscribeAuth();
         });
     }
 
+    /**
+     * An awaitable method (usable by a router guard) to determine if the first
+     * load of the sign-in state has occurred.
+     */
+    public async waitForAuthInit(): Promise<void> {
+        // If it’s already loaded, return a resolved promise:
+        if (this.isLoaded()) {
+            return;
+        }
+
+        // Otherwise, wait until the effect above emits on _loadedSubject:
+        await firstValueFrom(this._loadedWaiter);
+    }
+
+    /**
+     * Initiates the Google sign-in popup that signs in the user.
+     */
     public async signIn(): Promise<void> {
         const provider = new GoogleAuthProvider();
         await signInWithPopup(this._auth, provider);
     }
 
+    /**
+     * Signs-out the user.
+     */
     public async signOut(): Promise<void> {
         await signOut(this._auth);
         this._router.navigate(['/', 'admin', '403']);
+    }
+
+    /**
+     * If a route transition was unsuccessful, a route guard can use this method
+     * to store the `RouterStateSnapshot`'s `.url` property here. Successful
+     * sign-in later will allow the user to redirect to this page.
+     * @param url The URL to redirect to upon successful sign-in.
+     */
+    public setRedirectUrl(url: string): void {
+        this._redirectUrl = url;
     }
 }
