f27f425ee4188c1307ed4dcdfbd23ac482c739a3
nickbelling <6581760+nickbelling@users.noreply.github.com>
2025-01-04 01:47:59 +1100

    Screenshot in the Dark finished

--- src/app/games/screenshot-in-the-dark/controller.ts
+++ src/app/games/screenshot-in-the-dark/controller.ts
@@ -1,7 +1,12 @@
 import { Component, inject } from '@angular/core';
+import { Entity } from '../../common/firestore';
 import { BaseController, CommonControllerModule } from '../base/controller';
 import { ScreenshotInTheDarkDatabase } from './database';
-import { ScreenshotInTheDarkQuestion, ScreenshotInTheDarkState } from './model';
+import {
+    SCREENSHOT_IN_THE_DARK_BASE_PATH,
+    ScreenshotInTheDarkQuestion,
+    ScreenshotInTheDarkState,
+} from './model';
 import { ScreenshotInTheDarkQuestionEditDialog } from './question-edit';
 
 @Component({
@@ -25,12 +30,59 @@ export class ScreenshotInTheDarkController extends BaseController<
     }
 
     public async confirmDeleteQuestion(
-        question: ScreenshotInTheDarkQuestion,
-    ): Promise<void> {}
+        question: Entity<ScreenshotInTheDarkQuestion>,
+    ): Promise<void> {
+        this._confirm.open(
+            'deleteCancel',
+            'Delete question',
+            `Are you sure you want to delete '${question.gameId}'?`,
+            {
+                onDelete: async () => {
+                    const guessTheGameId = question.guessTheGameId;
+
+                    if (guessTheGameId) {
+                        const baseUrl = `${SCREENSHOT_IN_THE_DARK_BASE_PATH}/${guessTheGameId}_`;
+                        const deletions = [
+                            this.deleteFile(baseUrl + 1),
+                            this.deleteFile(baseUrl + 2),
+                            this.deleteFile(baseUrl + 3),
+                            this.deleteFile(baseUrl + 4),
+                            this.deleteFile(baseUrl + 5),
+                            this.deleteFile(baseUrl + 6),
+                        ];
+
+                        await Promise.all(deletions);
+                    }
+
+                    await this.deleteQuestion(question);
+                },
+            },
+        );
+    }
 
     public async setQuestion(questionId: string): Promise<void> {
         await this.setState({
             currentQuestion: questionId,
+            isPlaying: false,
+            isShowingAnswer: false,
+        });
+    }
+
+    public async play(): Promise<void> {
+        await this.setState({
+            isPlaying: true,
+        });
+    }
+
+    public async pause(): Promise<void> {
+        await this.setState({
+            isPlaying: false,
+        });
+    }
+
+    public async showAnswer(): Promise<void> {
+        await this.setState({
+            isShowingAnswer: true,
         });
     }
 }
