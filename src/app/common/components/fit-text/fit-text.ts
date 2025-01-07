9a19535c000b193f5d925f9ca8a6c0bc25940e07
nickbelling <6581760+nickbelling@users.noreply.github.com>
2025-01-08 01:06:42 +1100

    Added new component for fitting text horizontally

---
+++ src/app/common/components/fit-text/fit-text.ts
@@ -0,0 +1,10 @@
+import { Component, input } from '@angular/core';
+
+@Component({
+    selector: 'fit-text',
+    templateUrl: './fit-text.html',
+    styleUrl: './fit-text.scss',
+})
+export class FitText {
+    public readonly text = input.required<string>();
+}
