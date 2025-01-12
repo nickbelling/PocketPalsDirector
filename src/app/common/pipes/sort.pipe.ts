caaf2ceef00e7586f4451b85a1fbc3249e2c6303
nickbelling <6581760+nickbelling@users.noreply.github.com>
2025-01-13 01:45:31 +1100

    Added object property functionality to sort pipe

--- src/app/common/pipes/sort.pipe.ts
+++ src/app/common/pipes/sort.pipe.ts
@@ -8,11 +8,21 @@ import { Pipe, PipeTransform } from '@angular/core';
     pure: true,
 })
 export class SortPipe implements PipeTransform {
-    public transform<T extends string | number>(value: T[]): T[] {
-        return value.sort((a, b) => {
-            if (a < b) {
+    public transform<T extends string | number | Record<keyof T, unknown>>(
+        value: T[],
+        property?: keyof T,
+    ): T[] {
+        if (!Array.isArray(value)) {
+            return value;
+        }
+
+        return value.slice().sort((a, b) => {
+            const aValue = property ? a[property] : a;
+            const bValue = property ? b[property] : b;
+
+            if (aValue < bValue) {
                 return -1;
-            } else if (a > b) {
+            } else if (aValue > bValue) {
                 return 1;
             } else {
                 return 0;
