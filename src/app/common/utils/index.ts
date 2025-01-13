0c235fd3c504205a50a55dd42a5af746bba44e9d
nickbelling <6581760+nickbelling@users.noreply.github.com>
2025-01-13 23:41:20 +1100

    Random util

--- src/app/common/utils/index.ts
+++ src/app/common/utils/index.ts
@@ -2,6 +2,7 @@ export * from './color';
 export * from './equality';
 export * from './images';
 export * from './inject';
+export * from './random';
 export * from './sleep';
 export * from './sounds';
 export * from './text';
