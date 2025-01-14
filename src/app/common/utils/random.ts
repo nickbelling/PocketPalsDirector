64b3950a8f2d222a56dca628b798ebc68a635905
nickbelling <6581760+nickbelling@users.noreply.github.com>
2025-01-15 03:27:03 +1100

    Randomize items utility

--- src/app/common/utils/random.ts
+++ src/app/common/utils/random.ts
@@ -1,3 +1,12 @@
 export function getRandomItem<T>(array: T[]): T {
     return array[Math.floor(Math.random() * array.length)];
 }
+
+export function randomizeItems<T>(sorted: T[]): T[] {
+    const array = [...sorted];
+    for (let i = array.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1)); // random index from 0 to i
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+}
