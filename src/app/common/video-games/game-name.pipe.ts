2f53459c020ffec2b8c8aac0f6b0ed3acbbd1ab3
nickbelling <6581760+nickbelling@users.noreply.github.com>
2025-01-21 11:09:11 +1100

    gameName pipe now impure for easier use

--- src/app/common/video-games/game-name.pipe.ts
+++ src/app/common/video-games/game-name.pipe.ts
@@ -1,17 +1,37 @@
-import { computed, inject, Pipe, PipeTransform, Signal } from '@angular/core';
+import {
+    computed,
+    inject,
+    Pipe,
+    PipeTransform,
+    signal,
+    untracked,
+} from '@angular/core';
 import { VideogameDatabaseService } from './videogame-database-service';
 
 @Pipe({
     name: 'gameName',
-    pure: true,
+    pure: false,
 })
 export class VideogameNamePipe implements PipeTransform {
-    private _vgDb = inject(VideogameDatabaseService);
+    private readonly _vgDb = inject(VideogameDatabaseService);
+    private _slug = signal('');
 
-    public transform(slug: string): Signal<string> {
-        return computed(() => {
-            const games = this._vgDb.games();
-            return games.find((g) => g.id === slug)?.name || slug;
+    private readonly _gameName = computed(() => {
+        const games = this._vgDb.games();
+        const slug = this._slug();
+
+        const name = games.find((g) => g.id === slug)?.name || slug;
+        return name;
+    });
+
+    public transform(slug: string): string {
+        // Set the slug signal, which kicks off the computed _gameName signal if
+        // either the slug or the game list have changed
+        untracked(() => {
+            this._slug.set(slug);
         });
+
+        // Return the current result of the computed _gameName signal
+        return this._gameName();
     }
 }
