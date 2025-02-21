bbff7ae87ece011ec97024804b48249d7a90db2a
nickbelling <6581760+nickbelling@users.noreply.github.com>
2025-02-22 01:26:11 +1100

    Updated Avoiding the DMCA game to not change the displayed game/track when the question changes until the card flips back over.

--- src/app/games/avoiding-the-dmca/game.ts
+++ src/app/games/avoiding-the-dmca/game.ts
@@ -1,4 +1,11 @@
-import { Component, computed, effect, inject, viewChild } from '@angular/core';
+import {
+    Component,
+    computed,
+    effect,
+    inject,
+    signal,
+    viewChild,
+} from '@angular/core';
 import { fadeInOutAnimation } from '../../common/animations';
 import { AudioService, AudioVisualizer } from '../../common/audio';
 import { BaseGame, CommonGameModule } from '../base/game';
@@ -24,11 +31,32 @@ export class AvoidingTheDmcaGame extends BaseGame<
     protected baseUrl = 'games/avoiding-the-dmca/';
     protected muted = computed(() => !this._audio.audioEnabled());
 
+    /** True if the game card's back is currently showing. */
+    protected gameBackShowing = signal<boolean>(true);
+
+    /** True if the track card's back is currently showing. */
+    protected trackBackShowing = signal<boolean>(true);
+
+    /**
+     * The current question's game ID. Setting it is dependent upon the card
+     * flip animation being completed, so that it doesn't change before the card
+     * flips back over. See the `effect()` below.
+     */
+    protected gameId = signal<string | undefined>(undefined);
+
+    /**
+     * The current question's track name. Setting it is dependent upon the card
+     * flip animation being completed, so that it doesn't change before the card
+     * flips back over. See the `effect()` below.
+     */
+    protected trackName = signal<string | undefined>(undefined);
+
     constructor() {
         const db = inject(AvoidingTheDmcaDatabase);
         super(db);
         this._db = db;
 
+        // Starts or stops the backwards/forwards audio track.
         effect(() => {
             const state = this.state();
             const currentQuestion = this.currentQuestion();
@@ -51,6 +79,26 @@ export class AvoidingTheDmcaGame extends BaseGame<
                         forwards.stop();
                     }
                 }
+            }
+        });
+
+        // When the question changes, don't immediately change the game and
+        // track name - instead, wait for the card flip animation to finish.
+        effect(() => {
+            const currentQuestion = this.currentQuestion();
+            const gameBackShowing = this.gameBackShowing();
+            const trackBackShowing = this.trackBackShowing();
+
+            if (currentQuestion && gameBackShowing && trackBackShowing) {
+                this.gameId.set(currentQuestion.gameId);
+                this.trackName.set(currentQuestion.trackName);
+            } else if (
+                !currentQuestion &&
+                gameBackShowing &&
+                trackBackShowing
+            ) {
+                this.gameId.set(undefined);
+                this.trackName.set(undefined);
             }
         });
     }
